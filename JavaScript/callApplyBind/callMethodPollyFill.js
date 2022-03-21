961cf0c8df48a04f0f88ea2d10d3ac5764ab07a3
Naren-Mehta <deepakmehta.singh@gmail.com>
2022-03-21 18:35:43 +0530

    working on JS features

--- JavaScript/callApplyBind/callMethodPollyFill.js
+++ JavaScript/callApplyBind/callMethodPollyFill.js
@@ -1,19 +1,17 @@
-let person ={
+let person = {
     firstName: 'naren',
     lastName: 'mehta'
 };
 
-function getFullName(){
-    return this.firstName + ' ' + this.lastName;
+function getFullName(city, state) {
+    return this.firstName + ' ' + this.lastName, ' from ' + city + ' ' +state;
 }
 
-console.log(getFullName.call(person));
+console.log(getFullName.call(person, 'pith', 'uttarakhand'));
 
-Function.prototype.myCall = function(scope, ...args){
-        scope._fun = this;
-       return scope._fun(args);
+Function.prototype.myCall = function (scope, ...args) {
+    scope._fun = this;
+    return scope._fun(args);
 }
 
-console.log(getFullName.myCall(person));
-
-
+console.log(getFullName.myCall(person, 'pith', 'uttarakhand'));
